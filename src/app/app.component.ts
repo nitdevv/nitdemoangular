a4db36969fc534428d4f12a4029e081a1d8f646a
nitesh kumar singh <nitesh@excellencetechnologies.in>
2017-08-30 18:20:46 +0530

    formating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,27 @@ import {Component ,OnInit } from '@angular/core';
 
 import { Todo } from './todo';
 import { HeroService } from './todo.services';
+
 @Component({
   selector:'app-root',
   template:`
-  {{title}}
-<ul class="heroes">
-  <li *ngFor="let todo of HEROES">
-      {{todo.name}}
-        </li>
-        </ul>
+     {{title}}
+     <ul class="heroes">
+       <li *ngFor="let todo of HEROES">
+          {{todo.name}}
+       </li>
+     </ul>
   `,
-  providers:[HeroService]
+   providers:[HeroService]
   // styleUrls:['app/css/hero.component.css'],
 })
 export class AppComponent implements OnInit{
   HEROES: Todo[];
   constructor(private HeroService:HeroService){}
 
-getHeroes():void{
-  this.HeroService.getHeroes().then( HEROES=>this.HEROES= HEROES);
-}
+    getHeroes():void{
+      this.HeroService.getHeroes().then( HEROES=>this.HEROES= HEROES);
+  }
 ngOnInit():void{
   this.getHeroes();
 }
