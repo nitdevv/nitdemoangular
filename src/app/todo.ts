0293fb36c916314df4539bfa5ba4c3601703ebc3
nitesh kumar singh <nitesh@excellencetechnologies.in>
2017-09-07 19:04:14 +0530

    add todo.js file

---
+++ src/app/todo.ts
@@ -0,0 +1,11 @@
+// export class Todo {
+// 	constructor(
+// 	  public id: number,
+// 	  public name: string,
+// 	  public active: boolean = false,
+// 	  public complete: boolean = false) {}
+// }
+export class Todo {
+  id: number;
+  name: string;
+}
